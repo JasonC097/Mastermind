dc6365189102e7969d5eca3612bca0aaa990db7f
Jason Chung <jc097@bucknell.edu>
2024-03-06 19:51:53 -0500

    "HW#1, CodeMaker class mostly works still needs fixing"

--- src/main/java/hw01/CodeMaker.java
+++ src/main/java/hw01/CodeMaker.java
@@ -30,6 +30,13 @@ public class CodeMaker {
         // Call GameManager's generate solution to
         this.codeAnswer = GameManager.generateSolution();
     }
+    /**
+     * Constructor to help with JUnit tests
+     */
+    public CodeMaker(String manualCode){
+        this.codeAnswer = manualCode;
+    }
+
 
     /**
      * Goes through a user's guess and compares it to the answer
@@ -40,24 +47,38 @@ public class CodeMaker {
      */
     public String checkGuess(String breakGuess){
         String response = "";
-        int inCode = 0;
-        // Go through the user's guess to find numbers in the right spot or user's guess contains the right number but in the wrong spot
-        for (int i = 0; i < breakGuess.length(); i++){
-            if (breakGuess.charAt(i) == this.codeAnswer.charAt(i)){
+        String answerCopy = this.codeAnswer;
+        // Finds numbers in right spot first and exclude them in the copy of the answer
+        for (int i = 0; i < breakGuess.length(); i++) {
+            char checkNum = breakGuess.charAt(i);
+            if (checkNum == this.codeAnswer.charAt(i)) {
                 response = response.concat("*");
-            } else if (codeAnswer.contains(Character.toString(breakGuess.charAt(i)))) {
-                inCode += 1;
+                if (i + 1< breakGuess.length()) { //Ensure not out of bounds when skipping over number
+                answerCopy = answerCopy.substring(0,i) + answerCopy.substring(i+1);
+                }
+                else {
+                    answerCopy = answerCopy.substring(0,i);
+                }
+            }
+        }
+        // Checks to see if remaining numbers are in the guess but not in the right spot
+        for (int i = 0; i < breakGuess.length(); i++){
+            String checkNum = Character.toString(breakGuess.charAt(i));
+            if (answerCopy.contains(checkNum)) {
+                response = response.concat("+");
+                int locationOfNum = answerCopy.indexOf(checkNum);
+                if (locationOfNum + 1 < answerCopy.length()){
+                    answerCopy = answerCopy.substring(0, locationOfNum) + answerCopy.substring(locationOfNum + 1);
+                } else {
+                    answerCopy = answerCopy.substring(0, locationOfNum);
+                }
             }
         }
         // Fill the rest of response with numbers in the code with "+" and/or wrong numbers with "-"
-        for (int i = response.length(); i < breakGuess.length(); i++){
-            if (inCode > 0){
-                response = response.concat("+");
-                inCode -= 1;
-            } else {
-                response = response.concat("-");
-            }
+        for (int i = 0; i < answerCopy.length(); i++){
+            response = response.concat("-");
         }
+
         return response;
     }
 }
---
+++ src/test/java/hw01/CodeMakerTest.java
@@ -0,0 +1,39 @@
+package hw01;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CodeMakerTest {
+
+    private CodeMaker codeMaker;
+
+
+    @BeforeEach
+    void setUp() {
+        // Test answer being used
+        this.codeMaker = new CodeMaker("4613");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void checkGuess() {
+        String userGuess = "1331";
+        String expectedResponse = "++--";
+        String response = codeMaker.checkGuess(userGuess);
+        assertEquals(expectedResponse, response);
+        userGuess = "4231";
+        expectedResponse = "*++-";
+        response = codeMaker.checkGuess(userGuess);
+        assertEquals(expectedResponse, response);
+        userGuess = "1643";
+        expectedResponse = "**++";
+        response = codeMaker.checkGuess(userGuess);
+        assertEquals(expectedResponse, response);
+    }
+}
